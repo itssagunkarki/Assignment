dd308cd32c72a66c0fbf4c873a5453c31fdafb4c
Sagun Karki <sagunk064@gmail.com>
2023-04-11 18:33:01 -0500

    added invoices map to store class

--- src/com/fmt/Store.java
+++ src/com/fmt/Store.java
@@ -1,10 +1,13 @@
 package com.fmt;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class Store {
 	private String storeCode;
 	private Person managerCode;
 	private Address storeAddress;
+	private Map<String, Invoice> invoices = new HashMap<>();
 
 	/**
 	 * @param storeCode
@@ -12,20 +15,36 @@ public class Store {
 	 * @param storeAddress
 	 */
 	public Store(String storeCode, Person managerCode, Address storeAddress) {
+		if (storeCode == null || managerCode == null || storeAddress == null) {
+			throw new IllegalArgumentException("storeCode, managerCode, and storeAddress cannot be null");
+		}
+
 		this.storeCode = storeCode;
 		this.managerCode = managerCode;
 		this.storeAddress = storeAddress;
+		this.invoices = new HashMap<>(invoices);
 	}
 
 	public String getStoreCode() {
 		return storeCode;
 	}
 
-	public Person getManager() {
+	public Person getManagerCode() {
 		return managerCode;
 	}
 
 	public Address getStoreAddress() {
 		return storeAddress;
+	}
+
+	public Map<String, Invoice> getInvoices() {
+		return invoices;
+	}
+
+	public void addInvoice(Invoice invoice) {
+		if (invoice == null) {
+			throw new IllegalArgumentException("invoice cannot be null");
+		}
+		invoices.put(invoice.getInvoiceCode(), invoice);
 	}
 }
